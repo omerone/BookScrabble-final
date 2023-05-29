8d27085c4dab394f43e4bf3f8373196c92ef63f0
omerone <omermaoz1998@cs.colman.ac.il>
2023-05-29 17:28:13 +0300

    update

--- src/main/java/bookScrabble/model/communication/hostServer.java
+++ src/main/java/bookScrabble/model/communication/hostServer.java
@@ -7,49 +7,5 @@ import java.net.SocketTimeoutException;
 
 public class hostServer {
 
-    private final int port;
-    private final ClientHandler ch;
-    private volatile boolean stop;
 
-    public hostServer(int port, ClientHandler clientHandler){
-        this.port = port;
-        this.ch = clientHandler;
-        this.stop = false;
-    }
-
-    public void start (){
-        new Thread(()-> {
-            try {
-                runServer();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-    }
-
-    public void close() {
-        stop = true;
-    }
-
-    private void runServer()throws Exception{
-        ServerSocket server = new ServerSocket(port);
-        server.setSoTimeout(1000);
-        while (!stop) {
-            try {
-                Socket aClient = server.accept();
-                try {
-                    ch.handleClient(aClient.getInputStream(), aClient.getOutputStream());
-
-                    aClient.getInputStream();
-                    aClient.getOutputStream();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (SocketTimeoutException e) {
-                e.printStackTrace();
-            }
-        }
-        server.close();
-    }
 }
